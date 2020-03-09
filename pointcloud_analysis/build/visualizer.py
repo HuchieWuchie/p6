f8e9151f1ef409049eb295b1b8e5e4a0faf829bd
HuchieWuchie <albertdaugbjerg@gmail.com>
2020-03-09 14:42:53 +0100

    Removed some files

--- pointcloud_analysis/build/visualizer.py
+++ pointcloud_analysis/build/visualizer.py
@@ -1,12 +1,6 @@
 import numpy as np
 import open3d as o3d
 
-FILE_NAME_VERTS = "pointcloud.txt"
-npArr = np.loadtxt(fname = FILE_NAME_VERTS)
-verts = o3d.geometry.PointCloud()
-verts.points = o3d.utility.Vector3dVector(npArr)
-o3d.visualization.draw_geometries([verts])
-
 ## Visualize raw
 FILE_NAME_RAW = "04-01-test-scan-2.txt"
 npArr = np.loadtxt(fname = FILE_NAME_RAW)
